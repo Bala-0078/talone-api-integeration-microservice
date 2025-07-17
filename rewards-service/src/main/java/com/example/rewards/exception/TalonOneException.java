01787b7f416c05cf8e4c6b4eec091ad348381330
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-17 19:05:12 +0530

    Add or update file: TalonOneException.java

---
+++ rewards-service/src/main/java/com/example/rewards/exception/TalonOneException.java
@@ -0,0 +1,10 @@
+package com.example.rewards.exception;
+
+public class TalonOneException extends RuntimeException {
+    public TalonOneException(String message) {
+        super(message);
+    }
+    public TalonOneException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
