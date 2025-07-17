67fde5156476d5c368bbddb293616425c3bea28a
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-17 19:06:44 +0530

    Add or update file: UserLoginRequest.java

---
+++ src/main/java/com/example/userservice/dto/UserLoginRequest.java
@@ -0,0 +1,17 @@
+package com.example.userservice.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class UserLoginRequest {
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+
+    // Getters and Setters
+    public String getUsernameOrEmail() { return usernameOrEmail; }
+    public void setUsernameOrEmail(String usernameOrEmail) { this.usernameOrEmail = usernameOrEmail; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
