05d0d6fdbd61f38a931d712733a2b3ffd5c95864
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-17 19:05:09 +0530

    Add or update file: TalonOneSessionRequest.java

---
+++ rewards-service/src/main/java/com/example/rewards/model/TalonOneSessionRequest.java
@@ -0,0 +1,19 @@
+package com.example.rewards.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TalonOneSessionRequest {
+    private String integrationId;
+    private List<Map<String, Object>> cartItems;
+    private Double totalAmount;
+}
