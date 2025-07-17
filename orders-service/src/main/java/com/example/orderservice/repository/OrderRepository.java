916b53ed0cdb4a13a964641a6aa8c6144a304239
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-17 19:08:46 +0530

    Add or update file: OrderRepository.java

--- orders-service/src/main/java/com/example/orderservice/repository/OrderRepository.java
+++ orders-service/src/main/java/com/example/orderservice/repository/OrderRepository.java
@@ -2,8 +2,8 @@ package com.example.orderservice.repository;
 
 import com.example.orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<Order, UUID> {
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
