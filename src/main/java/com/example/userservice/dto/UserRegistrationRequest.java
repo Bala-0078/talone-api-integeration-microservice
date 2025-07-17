e58a4c4abafc4c31c1679fc464a166b755a0f690
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-17 19:06:41 +0530

    Add or update file: UserRegistrationRequest.java

---
+++ src/main/java/com/example/userservice/dto/UserRegistrationRequest.java
@@ -0,0 +1,27 @@
+package com.example.userservice.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserRegistrationRequest {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
