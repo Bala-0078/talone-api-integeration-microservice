e14b72ccbd9a6c514fd87a7479e51ddba55e323e
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-18 10:53:43 +0530

    Add or update file: RewardsServiceApplication.java

--- rewards-service/src/main/java/com/example/rewards/RewardsServiceApplication.java
+++ rewards-service/src/main/java/com/example/rewards/RewardsServiceApplication.java
@@ -2,10 +2,21 @@ package com.example.rewards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
+@EnableKafka
 public class RewardsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RewardsServiceApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Rewards Service API").version("1.0.0").description("API for evaluating discounts and managing customer rewards."));
     }
 }
