5ef713b8d8506e4924a2438c333121e49c5772ac
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-18 10:50:01 +0530

    Add or update file: UserNotFoundException.java

--- user-service/src/main/java/com/example/userservice/exception/UserNotFoundException.java
+++ user-service/src/main/java/com/example/userservice/exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.userservice.exception;
 
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends ApiException {
     public UserNotFoundException(String message) {
         super(message);
     }
