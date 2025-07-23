d3fd172751a5d85f87e77308549474edfea65358
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-07-23 14:38:18 +0530

    Add or update file: EmployeeController.java

---
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.employeemanagement.controller;
+
+import com.example.employeemanagement.model.Employee;
+import com.example.employeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
